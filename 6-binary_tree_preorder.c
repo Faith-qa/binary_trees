553ed14beeeeef88f9e1c3da54d1901b084b22e6
Faith-qa <atienofaith12@gmail.com>
2021-03-11 09:43:32 +0300

    corrected betty errors

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,14 +4,12 @@
  *binary_tree_preorder - goes through a binary using pre-order
  *@tree: pointer to root node
  *@func: pointer to a function to call each node
- *@int: value in the node
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
