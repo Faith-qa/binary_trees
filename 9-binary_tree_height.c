8bc1664618bef2cae7545fa2ae0db9093d807fbb
Faith-qa <atienofaith12@gmail.com>
2021-03-11 15:16:23 +0300

    corrected errors

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,21 +7,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t height_right;
+	size_t height_left;
+
 	if (tree == NULL)
 		return (0);
-	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-		size_t lh = binary_tree_height(tree->left) + 1;
-		size_t rh = binary_tree_height(tree->right) + 1;
+	height_left = binary_tree_height(tree->left) + 1;
+	height_right = binary_tree_height(tree->right) + 1;
 
-
-		if (lh > rh)
-			return (lh);
-
-		else
-			return (rh);
-	}
-
+	if (height_left > height_right)
+		return (height_left);
+	else
+		return (height_right);
 }
